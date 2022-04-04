1a94bda4c3159ddfae682d2dbb5c4b3316c78e7f
Muhammad Ammar <muhammadammar42000@gmail.com>
2022-04-05 03:27:42 +0500

    UserPage Completed 100%

--- Pages/UserPage/userPage.js
+++ Pages/UserPage/userPage.js
@@ -1,8 +1,5 @@
 $(document).ready(function () {
     $('.content').click(function () {
-        $('.content').toggleClass("heart-active")
-        $('.text').toggleClass("heart-active")
-        $('.numb').toggleClass("heart-active")
         $('.heart').toggleClass("heart-active")
     });
 });
